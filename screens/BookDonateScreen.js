ad69711a91d7e971b4035389e02f03ba7c2d7a4c
priyapandey2020 <priya.pandey@whitehatjr.com>
2020-10-16 18:33:43 +0530

    ..

---
+++ screens/BookDonateScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View,Text } from 'react-native';
+
+export default class BookDonateScreen extends Component{
+  render(){
+    return(
+      <View>
+        <text>
+          Book donate screen
+        </text>
+      </View>
+    )
+  }
+}
---
+++ screens/BookRequestScreen.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import {View,Text} from 'react-native';
+
+
+export default class BookRequestScreen extends Component{
+  render(){
+    return(
+      <View>
+        <text>
+          Book Request screen
+        </text>
+      </View>
+    )
+  }
+}
